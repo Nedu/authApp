e6a851d6605c45e29451a985b8bdd2e193e832c9
Nedu Robert <nedumrobert@gmail.com>
2019-09-18 09:16:45 +0100

    feat: authentication

---
+++ src/screens/Loading.js
@@ -0,0 +1,27 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text, ActivityIndicator, StyleSheet} from 'react-native';
+import firebase from 'react-native-firebase';
+
+export default class Loading extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Home' : 'Login');
+    });
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={{color: '#e93766', fontSize: 40}}>Loading</Text>
+        <ActivityIndicator color="#e93766" size="large" />
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
